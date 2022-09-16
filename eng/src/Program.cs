b422f9d69f8eaa380bf6ebcc177dbd9348b850da
Jan Hlavac <jan@postsharp.net>
2022-09-16 16:03:03 +0200

    Added MergePublisher to default publishers.

--- eng/src/Program.cs
+++ eng/src/Program.cs
@@ -7,10 +7,11 @@ using PostSharp.Engineering.BuildTools.Build.Publishers;
 using PostSharp.Engineering.BuildTools.Build.Solutions;
 using PostSharp.Engineering.BuildTools.Dependencies.Model;
 using Spectre.Console.Cli;
+using System.Linq;
 
-var product = new Product(Dependencies.MetalamaFrameworkExtensions)
+var product = new Product( Dependencies.MetalamaFrameworkExtensions )
 {
-    Solutions = new[] { new DotNetSolution("Metalama.Framework.Extensions.sln") { CanFormatCode = true } },
+    Solutions = new[] { new DotNetSolution( "Metalama.Framework.Extensions.sln" ) { CanFormatCode = true } },
     PublicArtifacts = Pattern.Create(
         "Metalama.Framework.DependencyInjection.$(PackageVersion).nupkg",
         "Metalama.Framework.DependencyInjection.ServiceLocator.$(PackageVersion).nupkg" ),
@@ -20,9 +21,7 @@ var product = new Product(Dependencies.MetalamaFrameworkExtensions)
         .WithValue(
         BuildConfiguration.Public, new BuildConfigurationInfo(
             MSBuildName: "Release",
-            PublicPublishers: new Publisher[] {
-                new MergePublisher()
-            } )
+            PublicPublishers: Product.DefaultPublicPublishers.Add( new MergePublisher() ).ToArray() )
         )
 };
 
