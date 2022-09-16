fcaf8393fc9944be59fdc4365dfe0bb0c5c8a1e4
Jan Hlavac <jan@postsharp.net>
2022-09-16 15:51:37 +0200

    Added MergePublisher to public build.

--- eng/src/Program.cs
+++ eng/src/Program.cs
@@ -1,7 +1,9 @@
 ﻿// Copyright (c) SharpCrafters s.r.o. See the LICENSE.md file in the root directory of this repository root for details.
 
 using PostSharp.Engineering.BuildTools;
+using PostSharp.Engineering.BuildTools.Build;
 using PostSharp.Engineering.BuildTools.Build.Model;
+using PostSharp.Engineering.BuildTools.Build.Publishers;
 using PostSharp.Engineering.BuildTools.Build.Solutions;
 using PostSharp.Engineering.BuildTools.Dependencies.Model;
 using Spectre.Console.Cli;
@@ -13,7 +15,15 @@ var product = new Product(Dependencies.MetalamaFrameworkExtensions)
         "Metalama.Framework.DependencyInjection.$(PackageVersion).nupkg",
         "Metalama.Framework.DependencyInjection.ServiceLocator.$(PackageVersion).nupkg" ),
     Dependencies = new[] { Dependencies.PostSharpEngineering, Dependencies.Metalama },
-    MainVersionDependency = Dependencies.Metalama
+    MainVersionDependency = Dependencies.Metalama,
+    Configurations = Product.DefaultConfigurations
+        .WithValue(
+        BuildConfiguration.Public, new BuildConfigurationInfo(
+            MSBuildName: "Release",
+            PublicPublishers: new Publisher[] {
+                new MergePublisher()
+            } )
+        )
 };
 
 var commandApp = new CommandApp();
