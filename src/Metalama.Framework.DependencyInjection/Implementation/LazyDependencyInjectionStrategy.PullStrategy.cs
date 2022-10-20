764c00e1f666d103add3f350fc79fc5ce9aa73c7
GaelFraiteur <gael@postsharp.net>
2022-10-20 13:34:09 +0200

    Update Metalama.

--- src/Metalama.Framework.DependencyInjection/Implementation/LazyDependencyInjectionStrategy.PullStrategy.cs
+++ src/Metalama.Framework.DependencyInjection/Implementation/LazyDependencyInjectionStrategy.PullStrategy.cs
@@ -17,7 +17,7 @@ public partial class LazyDependencyInjectionStrategy
         public PullStrategy( DependencyContext context, IProperty mainProperty, IField funcField ) : base( context, mainProperty )
         {
             this._funcField = funcField;
-            this._funcType = ((INamedType) TypeFactory.GetType( typeof(Func<>) )).ConstructGenericInstance( mainProperty.Type ).ConstructNullable();
+            this._funcType = ((INamedType) TypeFactory.GetType( typeof(Func<>) )).WithTypeArguments( mainProperty.Type ).ConstructNullable();
         }
 
         protected override IFieldOrProperty AssignedFieldOrProperty => this._funcField;
