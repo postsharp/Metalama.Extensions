63ee56b5d84b6146d2714bbc7bb956142d9d2549
GaelFraiteur <gael@postsharp.net>
2023-03-20 12:56:15 +0100

    Added repro for 32851.

---
+++ src/tests/Metalama.Extensions.Architecture.AspectTests/Bugs/Bug32851.Dependency.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) SharpCrafters s.r.o. See the LICENSE.md file in the root directory of this repository root for details.
+
+using Metalama.Extensions.Architecture.Aspects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metalama.Extensions.Architecture.AspectTests.Bugs.Bug32851
+{
+    [CanOnlyBeUsedFrom( Namespaces = new[] { "ClassLibrary2" }, ValidateDerivedTypes = true )]
+    public class InternalClass
+    {
+    }
+
+}
