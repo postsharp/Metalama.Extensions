8d5f80797c11ecfecc5bce774b72b1247b73d7b4
Petr Onderka <petro@postsharp.net>
2023-09-04 12:53:54 +0200

    #33721 Fix comments in test output

--- src/tests/Metalama.Extensions.Multicast.AspectTests/Filter_ByTypeName_Wildcards.t.cs
+++ src/tests/Metalama.Extensions.Multicast.AspectTests/Filter_ByTypeName_Wildcards.t.cs
@@ -1,12 +1,10 @@
 [assembly: AddTag("Prefixed", AttributeTargetElements = MulticastTargets.Class, AttributeTargetTypes = "Metalama.Extensions.Multicast.AspectTests.Filter_ByTypeName_Wildcards.Prefixed*")]
 [assembly: AddTag("Ns", AttributeTargetElements = MulticastTargets.Class, AttributeTargetTypes = "Metalama.Extensions.Multicast.AspectTests.Filter_ByTypeName_Wildcards.Ns.*")]
 [Tag("Prefixed")]
-// <target>
 public class PrefixedA
 {
 }
 [Tag("Prefixed")]
-// <target>
 public class PrefixedB
 {
 }
