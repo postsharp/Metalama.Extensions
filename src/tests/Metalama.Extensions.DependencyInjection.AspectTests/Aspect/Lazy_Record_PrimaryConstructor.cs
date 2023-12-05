29116079b6cdcec2338c6342f1e0865dc0f5b4c5
Petr Onderka <petro@postsharp.net>
2023-12-05 18:48:46 +0100

    #33923 Make DI work for records

---
+++ src/tests/Metalama.Extensions.DependencyInjection.AspectTests/Aspect/Lazy_Record_PrimaryConstructor.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) SharpCrafters s.r.o. See the LICENSE.md file in the root directory of this repository root for details.
+
+using Metalama.Backstage.Diagnostics;
+
+namespace Metalama.Extensions.DependencyInjection.AspectTests.Aspect.Lazy_Record_PrimaryConstructor;
+
+// <target>
+public record TargetRecord()
+{
+    [Dependency( IsLazy = true )]
+    private readonly ILogger _logger;
+}
