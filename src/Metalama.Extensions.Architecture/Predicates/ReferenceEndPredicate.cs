d4572c1a8c617e09b26e1f846495f94fad6bc9cd
GaelFraiteur <gael@postsharp.net>
2024-04-18 10:32:37 +0200

    Update engineering.

--- src/Metalama.Extensions.Architecture/Predicates/ReferenceEndPredicate.cs
+++ src/Metalama.Extensions.Architecture/Predicates/ReferenceEndPredicate.cs
@@ -1,8 +1,6 @@
 // Copyright (c) SharpCrafters s.r.o. See the LICENSE.md file in the root directory of this repository root for details.
 
 using Metalama.Framework.Validation;
-using System;
-using System.Diagnostics;
 
 namespace Metalama.Extensions.Architecture.Predicates;
 
@@ -15,10 +13,7 @@ public abstract class ReferenceEndPredicate : ReferencePredicate
         this.Direction = builder.Direction;
     }
 
-    public override bool IsMatch( ReferenceValidationContext context )
-    {
-        return this.IsMatch( context.GetReferenceEnd( this.Direction ) );
-    }
+    public sealed override bool IsMatch( ReferenceValidationContext context ) => this.IsMatch( context.GetReferenceEnd( this.Direction ) );
 
     public abstract bool IsMatch( in ReferenceEnd referenceEnd );
 }
